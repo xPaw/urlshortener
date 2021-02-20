4e0425045fb2d8d21c67909d1d37a4d089c1cc95
Pavel Djundik <xPaw@users.noreply.github.com>
2021-02-20 12:23:50 +0200

    Add config

---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+define( 'CONFIG_HOSTNAME', 'example.com' );
+define( 'CONFIG_CREATE_SECRET', 'secret-token:SOME-COOL-SECRET-TOKEN-GOES-HERE' );
+
+define( 'CONFIG_HASHIDS_SALT', 'very-cool-hashids-salt' );
+define( 'CONFIG_HASHIDS_LENGTH', 8 );
+
+define( 'CONFIG_PDO_DSN', 'mysql:host=localhost;dbname=example;charset=utf8mb4' );
+define( 'CONFIG_PDO_USER', 'root' );
+define( 'CONFIG_PDO_PASS', 'root' );
